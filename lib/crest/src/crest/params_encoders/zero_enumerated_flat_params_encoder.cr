5ee84134f517791965cbe7b869bc4d5f2c08c16b
Rodrigo de Avila <rdeavila@gmail.com>
2024-09-25 17:09:16 +0000

    Update script

--- lib/crest/src/crest/params_encoders/zero_enumerated_flat_params_encoder.cr
+++
@@ -1,5 +0,0 @@
-module Crest
-  class ZeroEnumeratedFlatParamsEncoder < EnumeratedFlatParamsEncoder
-    class_getter array_start_index = 0
-  end
-end
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Função para calcular o tempo decorrido em palavras (adaptada para o Bash)
 time_ago_in_words() {
     from_time=$1
     to_time=$2
@@ -31,7 +30,6 @@ time_ago_in_words() {
 
 echo "Collecting time from primary mirrors..."
 
-# Usando curl para obter o tempo dos espelhos (substituindo Crest)
 atl_resp=$(curl -s -k "https://atl.rsync.repo.almalinux.org/almalinux/TIME")
 sea_resp=$(curl -s -k "https://sea.rsync.repo.almalinux.org/almalinux/TIME")
 
@@ -44,13 +42,10 @@ else
 fi
 
 echo "Collecting mirror list..."
-
-# Usando curl e jq para obter e processar a lista de espelhos
 all_mirrors=$(curl -s -k "https://mirrors.almalinux.org/debug/json/all_mirrors")
 mirrorlist=$(echo "$all_mirrors" | jq '.result')
 echo " done."
 
-# Usando heredoc para construir o Markdown
 cat <<MD > docs/index.md
 ---
 hide:
@@ -63,8 +58,8 @@ hide:
 
 This service provides information about the status of the AlmaLinux mirrors. The report shows the time it takes for updates to propagate to the mirrors, as well as the number of mirrors that have been updated. This information can be used to identify mirrors that are not up to date, and to troubleshoot any problems with the mirror propagation process.
 
-- Source mirror address: `rsync.repo.almalinux.org`
-- Source mirror build date: `$(date -d @$original_time)`
+- Source mirror address: \`rsync.repo.almalinux.org\`
+- Source mirror build date: \`$(date -d @$original_time)\`
 
 MD
 
@@ -72,70 +67,69 @@ mirrorlist_total=$(echo "$mirrorlist" | jq 'keys | length')
 mirrorlist_completed=0
 
 echo "Starting mirror probe..."
+in_sync=""
+behind=""
+unavailable=""
 
-# Iterando sobre os espelhos usando jq
 echo "$mirrorlist" | jq -r 'keys[]' | while read -r mirror; do
-    status=$(echo "$mirrorlist" | jq -r ".[$mirror].status")
-    private=$(echo "$mirrorlist" | jq -r ".[$mirror].private")
+    status=$(echo "$mirrorlist" | jq -r --arg m "$mirror" '.[$m].status')
+    private=$(echo "$mirrorlist" | jq -r --arg m "$mirror" '.[$m].private')
 
     if [[ $status == "ok" && $private == false ]]; then
-        sponsor=$(echo "$mirrorlist" | jq -r ".[$mirror].sponsor_name")
-        sponsor_url=$(echo "$mirrorlist" | jq -r ".[$mirror].sponsor_url")
+        sponsor=$(echo "$mirrorlist" | jq -r  --arg m "$mirror" '.[$m].sponsor_name')
+        sponsor_url=$(echo "$mirrorlist" | jq -r --arg m "$mirror" '.[$m].sponsor_url')
 
-        if echo "$mirrorlist" | jq -e ".[$mirror].urls.http" >/dev/null; then
-            mirror_address=$(echo "$mirrorlist" | jq -r ".[$mirror].urls.http")
+        if echo "$mirrorlist" | jq -e --arg m "$mirror" '.[$m].urls.http' >/dev/null; then
+            mirror_address=$(echo "$mirrorlist" | jq -r --arg m "$mirror" '.[$m].urls.http')
         else
-            mirror_address=$(echo "$mirrorlist" | jq -r ".[$mirror].urls.https")
+            mirror_address=$(echo "$mirrorlist" | jq -r --arg m "$mirror" '.[$m].urls.https')
         fi
 
         if [[ ! $mirror_address =~ /$ ]]; then
             mirror_address="$mirror_address/"
         fi
 
-        # Usando curl com timeout para obter o tempo do espelho
-        mirror_resp=$(curl -s -k -m 2 -max-time 2 --max-redirs 2 "$mirror_address/TIME")
+        mirror_resp=$(curl -s -L --max-time 2 --max-redirs 2 "$mirror_address/TIME")
 
         if [[ $? -eq 0 ]]; then
             mirror_time=$mirror_resp
             compare=$((original_time - mirror_time))
 
             if [[ $compare -le 0 ]]; then
-                in_sync+="    | $mirror | [$sponsor]($sponsor_url) |\n"
+                in_sync="${in_sync}    | ${mirror} | \[${sponsor}\]\(${sponsor_url}\) |\n"
             else
-                behind+="    | $mirror | [$sponsor]($sponsor_url) | $(time_ago_in_words $mirror_time $original_time) |\n"
+                behind="${behind}    | ${mirror} | \[${sponsor}\]\(${sponsor_url}\) | $(time_ago_in_words "${mirror_time}" "${original_time}") |\n"
             fi
         else
-            unavailable+="    | $mirror | [$sponsor]($sponsor_url) | curl error |\n" 
+            unavailable="${unavailable}    | ${mirror} | \[${sponsor}\]\(${sponsor_url}\) | curl error |\n" 
         fi
     fi
     mirrorlist_completed=$((mirrorlist_completed + 1))
 
-    echo "  $((mirrorlist_completed / mirrorlist_total * 100))%"
+    echo "Tested $mirrorlist_completed of $mirrorlist_total"
 done
 
-# Adicionando as tabelas ao Markdown
 cat <<MD >> docs/index.md
-
 === "In sync"
 
     | Mirror Name | Sponsor |
     |:--|:--|
-$in_sync
+${in_sync}
 
 === "Behind primary"
 
     | Mirror Name | Sponsor | Time behind primary |
     |:--|:--|:--|
-$behind
+${behind}
 
 === "Unavailable"
 
     | Mirror Name | Sponsor | Reason |
     |:--|:--|:--|
-$unavailable
+${unavailable}
 
 
-Last report update: `$(date -u)`
+Last report update: \`$(date -u)\`
 
 MD
 
